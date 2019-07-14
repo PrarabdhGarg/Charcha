adf2511de138c2414c0383f9e0fc3e7cf92525ec
Prarabdh Garg <prarabdhgrg@gmail.com>
2019-07-14 19:06:12 +0530

    Add config file
    The config file contains a set of static variables that are needed
    throughout the app. This will be removed once caching is implemented in
    the app

---
+++ lib/config.dart
@@ -0,0 +1,10 @@
+import 'package:firebase_auth/firebase_auth.dart';
+
+import 'dataClasses.dart';
+
+class config {
+  static String baseUrl = "https://dush-t-pprvoice.herokuapp.com";
+  static String jwt = "";
+  static FirebaseUser firebaseUser;
+  static User userProfile;
+}

1b3123e8c054b7b8a95d4d4eadf79af40eeb7847
Prarabdh Garg <prarabdhgrg@gmail.com>
2019-07-16 01:12:11 +0530

    Improve UI for feed card

--- lib/feed.dart
+++ lib/feed.dart
@@ -1,82 +1,131 @@
 import 'package:audioplayer/audioplayer.dart';
+import 'package:material_design_icons_flutter/material_design_icons_flutter.dart';
 import 'package:charcha/dataClasses.dart';
 import 'package:flutter/material.dart';
 
 import 'audioControl.dart';
 
 Widget getFeedListWidget(List<feedModel> items , BuildContext context) {
-  return ListView.builder(
-    itemCount: items.length,
-    itemBuilder: (BuildContext context , int i) {
-      return Row(
-        children: <Widget>[
-          Container(
-            width: 100,
-            height: 100,
-            margin: EdgeInsets.all(16),
-            decoration: new BoxDecoration(
-              shape: BoxShape.circle,
-              image: DecorationImage(image: AssetImage('images/profile.png')),
-            ),
+  return Container(
+    color: Colors.grey,
+    child: ListView.builder(
+      itemCount: items.length,
+      itemBuilder: (BuildContext context , int i) {
+        return Container(
+          padding: EdgeInsets.all(8),
+          margin: EdgeInsets.all(8),
+          decoration: new BoxDecoration(
+            borderRadius: BorderRadius.all(Radius.circular(16)),
+            color: Colors.white,
           ),
-          Flexible(
-            flex: 3,
-            fit: FlexFit.tight,
-            child: Column(
-              children: <Widget>[
-                Center(
-                  child: Text(items[i].title),
-                ),
-                Center(
-                  child: Text(items[i].description),
-                ),
-                audioControl(items[i].url),
-                Row(
-                  children: <Widget>[
-                    Expanded(
-                      flex: 1,
-                      child: Container(
-                        child: RawMaterialButton(
-                          child: Text("Like"),
-                          fillColor: Colors.cyan,
-                          onPressed: null,
-                          materialTapTargetSize:
-                          MaterialTapTargetSize.shrinkWrap,
-                        ),
-                        margin: EdgeInsets.all(5.0),
-                      )),
-                    Expanded(
-                      flex: 1,
-                      child: Container(
-                        child: RawMaterialButton(
-                          child: Text("Comment"),
-                          fillColor: Colors.cyan,
-                          onPressed: null,
-                          materialTapTargetSize:
-                          MaterialTapTargetSize.shrinkWrap,
-                        ),
-                        margin: EdgeInsets.all(5.0),
-                      )),
-                    Expanded(
-                      flex: 1,
-                      child: Container(
-                        child: RawMaterialButton(
-                          child: Text("Share"),
-                          onPressed: null,
-                          fillColor: Colors.cyan,
-                          materialTapTargetSize:
-                          MaterialTapTargetSize.shrinkWrap,
-                        ),
-                        margin: EdgeInsets.all(5.0),
-                      )
+          child: Row(
+            children: <Widget>[
+              Stack(
+                children: <Widget>[
+                  Container(
+                    width: 100,
+                    height: 100,
+                    margin: EdgeInsets.all(16),
+                    decoration: new BoxDecoration(shape: BoxShape.circle,color: Colors.white)
+                  ),
+                  Positioned(
+                    top: 10,
+                    left: 10,
+                    child: Container(
+                      width: 80,
+                      height: 80,
+                      margin: EdgeInsets.all(16),
+                      decoration: new BoxDecoration(
+                        shape: BoxShape.circle,
+                        image: DecorationImage(image: AssetImage('images/nature.jpg')),
+                      ),
                     ),
+                  ),
+                ],
+              ),
+              Flexible(
+                flex: 3,
+                fit: FlexFit.tight,
+                child: Column(
+                  children: <Widget>[
+                    Row(
+                      children: <Widget>[
+                        Flexible(
+                          fit: FlexFit.loose,
+                          child: Text(items[i].title, style: TextStyle(fontWeight: FontWeight.w900),),
+                        ),
+                        VerticalDivider(),
+                        Flexible(
+                          flex: 1,
+                          child: Text(items[i].userName, style: TextStyle(color: Colors.lightBlue, fontSize: 12),),
+                        )
+                      ],
+                    ),
+                    Container(height: 8,),
+                    Center(
+                      child: Text(items[i].description, overflow: TextOverflow.ellipsis, maxLines: 3,),
+                    ),
+                    Container(height: 8,),
+                    // audioControl(items[i].url),
+                    Row(
+                      children: <Widget>[
+                        Expanded(
+                          flex: 1,
+                          child: Row(
+                            children: <Widget>[
+                              Flexible(
+                                flex: 1,
+                                child: Icon(Icons.thumbs_up_down , color: Color(0xFFFF6969),)
+                              ),
+                              Container(width: 4,),
+                              Flexible(
+                                flex: 1,
+                                child: Text("01" , style: TextStyle(color: Colors.grey),),
+                              )
+                            ],
+                          ) 
+                        ),
+                        Expanded(
+                            flex: 1,
+                            child: Row(
+                              children: <Widget>[
+                                Flexible(
+                                    flex: 1,
+                                    child: Icon(Icons.add_comment , color: Color(0xFFFF6969),)
+                                ),
+                                Container(width: 4,),
+                                Flexible(
+                                  flex: 1,
+                                  child: Text("01" , style: TextStyle(color: Colors.grey),),
+                                )
+                              ],
+                            )
+                        ),
+                        Expanded(
+                            flex: 1,
+                            child: Row(
+                              children: <Widget>[
+                                Flexible(
+                                    flex: 1,
+                                    child: Icon(Icons.forward , color: Color(0xFFFF6969),)
+                                ),
+                                Container(width: 4,),
+                                Flexible(
+                                  flex: 1,
+                                  child: Text("01" , style: TextStyle(color: Colors.grey),),
+                                )
+                              ],
+                            )
+                        ),
+                      ],
+                    )
                   ],
                 )
-              ],
-            )
-          )
-        ],
-      );
-    }
+              )
+            ],
+          ),
+        );
+      }
+    ),
   );
 }

2a12551114275fe20300b71e91fb2363f917e25d
Prarabdh Garg <prarabdhgrg@gmail.com>
2019-07-17 14:17:13 +0530

    Update feed model according to data that is to be displayed

--- lib/dataClasses.dart
+++ lib/dataClasses.dart
@@ -4,7 +4,7 @@ import 'package:flutter/material.dart';
 
 class feedModel {
   ImageProvider thumbnail = AssetImage('images/profile.png');
-  String name = "Default Name";
+  String userName = "Default Name";
   String title = "Default Title";
   String description = "This is a description that can be at max 120 charecters long";
   String url = "https://dl.espressif.com/dl/audio/ff-16b-2c-44100hz.mp4";
